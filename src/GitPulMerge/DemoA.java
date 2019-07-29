9de3f9d06644f660c89da3596d781063b27dbdeb
ashitha123456 <ashithashetty064@gmail.com>
2019-07-29 16:37:19 +0530

    first commit

--- src/GitPulMerge/DemoA.java
+++ src/GitPulMerge/DemoA.java
@@ -1,5 +1,7 @@
 package GitPulMerge;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,10 +22,18 @@ public class DemoA {
 
 		WebElement ele = driver.findElement(By.xpath("//li[@class='menuparent gold-coins']"));
 		act.moveToElement(ele).build().perform();
+		
+		
 		driver.findElement(By.xpath("//span[@data-p='gold-coins-weight-20gms,m']")).click();
-		Thread.sleep(2000);
+	
 		Assert.assertEquals(driver.getTitle(), "20 gram 24 KT Gold Coin | BlueStone.com");
 		driver.close();
+		WebElement ele1=driver.findElement(By.xpath("//section[@id='Delivery Time-form']/span[@class='title style-fill i-right']"));
+	    act.moveToElement(ele).build().perform();
+	    driver.findElement(By.xpath("//div[@class='form-item ']/span[@data-tagcategory='Delivery Time']")).click();
+	    List<WebElement> ele2=driver.findElements(By.xpath("//ul[@id='product_list_ui']/li"));
+	    System.out.println(ele2.size());
+	    
 	}
 
 }
